451d723c6bb266d82da57296d47f5b0d6745a1c1
Baiba Apkalna <apkalna@gmail.com>
2021-09-04 18:52:16 +0300

    Week 2 Workshop Assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const Campsite = require('../models/campsite');
 
 const campsiteRouter = express.Router();
-
-campsiteRouter.use(bodyParser.json());
 
 campsiteRouter.route('/')
 .get((req, res, next) => {
